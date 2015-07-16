d29c6b3234134e353b68ae21933f555c40f3a602
iero <greg@iero.org>
2015-07-16 23:03:45 +0200

    Watchdog & uptime support
    That works !

--- Yun/CheapWatchDog/CheapWatchDog.ino
+++ Yun/CheapWatchDog/CheapWatchDog.ino
@@ -3,20 +3,15 @@
 #define PULSE_PIN 11 // Get Heartbeat
 #define RESET_PIN 9 // Port to send reset
 
-#define WATCHDOG_DELAY 5000 // Wait x ms before sending reset
-#define ARDUINO_REBOOT_TIME 10000 // Wait x ms for master Arduino to restart
+#define WATCHDOG_DELAY 10000 // Wait 10s before sending reset
+#define ARDUINO_REBOOT_TIME 60000 // Wait 1 min for master Arduino to restart
 
 unsigned long lastHeartbeat = 0;
 unsigned long lastUptimeReport = 0;
 
-void setup() {
-    // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);  
-  Serial.println("Arduino reset");
-  
-  // put your setup code here, to run once:
+void setup() {  
   digitalWrite(LED_PIN, HIGH);
-  delay(2000) ;
+  delay(ARDUINO_REBOOT_TIME);
   lastHeartbeat = millis();
   digitalWrite(LED_PIN, LOW);
 }
@@ -24,31 +19,34 @@ void setup() {
 void loop() {
   // Get last pulse
   boolean pulse = digitalRead(PULSE_PIN);
-  if (pulse == HIGH) {
+
+  if (pulse == HIGH) { // Got pulse
+    digitalWrite(LED_PIN, HIGH);
     lastHeartbeat = millis();
-    Serial.println("Pulse !");
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
   }
-  
+
   unsigned long uptime = millis();
   if ((uptime - lastHeartbeat) >= WATCHDOG_DELAY) { //too long
-    Serial.println("Uptime: " + String((uptime - (uptime % 5000)) / 1000) + " seconds (" + String((uptime - lastHeartbeat) / 1000) + " seconds since last heartbeat)");
+    //Serial.println("Uptime: " + String((uptime - (uptime % 5000)) / 1000) + " seconds (" + String((uptime - lastHeartbeat) / 1000) + " seconds since last heartbeat)");
     sendReset();
-
   }
-  delay(2000); // needed for prod ?
+  //delay(200); // needed for prod ?
 }
 
 void sendReset() {
-  // Send reset to main Arduino
-  Serial.println("Reset.. and wait");
+  // Send reset to main Arduino by sending LOW
+  //Serial.println("Reset.. and wait");
   digitalWrite(LED_PIN, HIGH);
-  digitalWrite(RESET_PIN, HIGH);
-  delay(1000);
   digitalWrite(RESET_PIN, LOW);
+  delay(1000);
+  digitalWrite(RESET_PIN, HIGH);
   // Wait Main arduino to reboot
   delay(ARDUINO_REBOOT_TIME);
   digitalWrite(LED_PIN, LOW);
   delay(5000);
   lastHeartbeat = millis();
-  Serial.println("Back on line ?");
+  //Serial.println("Back on line ?");
 }
---
+++ Yun/ieroYunWatchDogUptime/ieroYunWatchDogUptime.ino
@@ -0,0 +1,137 @@
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+#include <dht.h>
+
+// Radiateurs
+#define SWITCH_2F_PIN 2   // Chambre Parents 2 kW et SDB RDC 1 kW
+#define SWITCH_1G_PIN 3   // Cuisine 0.9 kW et SDB Etage 1 kW
+#define SWITCH_3H_PIN 4   // Salon - cuisine 2 kW
+#define SWITCH_3I_PIN 5   // Salon - videoproj 1.5 kW
+#define SWITCH_2C_PIN 6  // Salon - salle a manger 1.5 kW
+#define SWITCH_3C_PIN 7  // Entree 0.9 kW
+
+#define SWITCH_2J_PIN 8   // Chauffe eau
+//#define SWITCH_3A_PIN 9   // Lumiere jardin
+int MAX_OUTPUT = 9 ; // Last pin used for output
+
+#define DHT22_PIN 10 // Temp et Hum
+#define WATCHDOG_PIN 11 // Heartbeat for WatchDog
+#define LED_PIN 13 // LED for heart beat
+
+#define PULSE_DELAY 500 // delay for "for loop" (ms)
+
+boolean SWITCH_STATE[13] ; // State of relays. true if ON.
+
+void readTemperature(YunClient client);
+void readTHumidity(YunClient client);
+void switchON(int PIN) ;
+void switchOFF(int PIN) ;
+boolean switchSTATE(int PIN) ;
+void resetAllSwitches() ;
+
+YunServer server;
+dht DHT;
+
+void setup() {
+  Bridge.begin();
+  server.listenOnLocalhost();
+  server.begin();
+  
+  for (int i=2; i <= MAX_OUTPUT; i++){ 
+    pinMode(i,OUTPUT);
+  }
+    
+  resetAllSwitches();
+}
+
+void loop() {
+
+  YunClient client = server.accept();
+  if (client) {
+    String command = client.readString();
+    command.trim();
+    if (command == "temperature") readTemperature(client);
+    else if (command == "humidity") readHumidity(client);
+
+    else if (command == "All/Off") resetAllSwitches();
+
+    else if (command == "2F/On") switchON(SWITCH_2F_PIN);
+    else if (command == "2F/Off") switchOFF(SWITCH_2F_PIN);
+    else if (command == "2F/Status") client.print(switchSTATE(SWITCH_2F_PIN));
+
+    else if (command == "1G/On") switchON(SWITCH_1G_PIN);
+    else if (command == "1G/Off") switchOFF(SWITCH_1G_PIN);
+    else if (command == "1G/Status") client.print(switchSTATE(SWITCH_1G_PIN));
+
+    else if (command == "3H/On") switchON(SWITCH_3H_PIN);
+    else if (command == "3H/Off") switchOFF(SWITCH_3H_PIN);
+    else if (command == "3H/Status") client.print(switchSTATE(SWITCH_3H_PIN));
+
+    else if (command == "3I/On") switchON(SWITCH_3I_PIN);
+    else if (command == "3I/Off") switchOFF(SWITCH_3I_PIN);
+    else if (command == "3I/Status") client.print(switchSTATE(SWITCH_3I_PIN));
+
+    else if (command == "2C/On") switchON(SWITCH_2C_PIN);
+    else if (command == "2C/Off") switchOFF(SWITCH_2C_PIN);
+    else if (command == "2C/Status") client.print(switchSTATE(SWITCH_2C_PIN));
+
+    else if (command == "3C/On") switchON(SWITCH_3C_PIN);
+    else if (command == "3C/Off") switchOFF(SWITCH_3C_PIN);
+    else if (command == "3C/Status") client.print(switchSTATE(SWITCH_3C_PIN));
+
+    else if (command == "2J/On") switchON(SWITCH_2J_PIN);
+    else if (command == "2J/Off") switchOFF(SWITCH_2J_PIN);
+    else if (command == "2J/Status") client.print(switchSTATE(SWITCH_2J_PIN));
+
+    //else if (command == "3A/On") switchON(SWITCH_3A_PIN);
+    //else if (command == "3A/Off") switchOFF(SWITCH_3A_PIN);
+    //else if (command == "3A/Status") client.print(switchSTATE(SWITCH_3A_PIN));
+
+    else if (command == "uptime") client.print(millis());
+
+    client.stop();
+  } 
+  
+  // Send watchdog signal and wait before continuing
+  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(WATCHDOG_PIN, HIGH);
+  delay(PULSE_DELAY); // wait enough time to allow watchdog to see the signal
+  digitalWrite(WATCHDOG_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  delay(PULSE_DELAY); // wait enough time to allow watchdog to see the signal
+}
+
+void readTemperature(YunClient client) {
+  DHT.read22(DHT22_PIN);
+  client.print(DHT.temperature); 
+}
+
+void readHumidity(YunClient client) {
+  DHT.read22(DHT22_PIN);
+  client.print(DHT.humidity); 
+}
+
+void switchON(int PIN) {
+  digitalWrite(PIN, HIGH);
+  SWITCH_STATE[PIN]=true;
+}
+
+void switchOFF(int PIN) {
+  digitalWrite(PIN, LOW);
+  SWITCH_STATE[PIN]=false;
+}
+
+boolean switchSTATE(int PIN) {
+  return SWITCH_STATE[PIN];
+}
+
+void resetAllSwitches() {
+  for (int i=2; i <= MAX_OUTPUT; i++){ 
+      switchOFF(i);
+  }
+  digitalWrite(WATCHDOG_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+}
+
+
